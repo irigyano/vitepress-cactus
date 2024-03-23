52092387519b68fb1f25817746ea2441b6f5f62f
irigyano <110440243+irigyano@users.noreply.github.com>
2024-03-24 04:23:08 +0800

    chore: replace node with bun in vite

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,4 +5,17 @@ export default defineConfig({
   cleanUrls: true,
   title: "iriwrap",
   description: "A VitePress Site",
+  // // to override theme use: bun run dev:bun
+  // vite: {
+  //   resolve: {
+  //     alias: [
+  //       {
+  //         find: /^.*\/VPNavBar\.vue$/,
+  //         replacement: Bun.fileURLToPath(
+  //           new URL('./components/CustomNavBar.vue', import.meta.url)
+  //         )
+  //       }
+  //     ]
+  //   }
+  // }
 });
