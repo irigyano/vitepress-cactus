0dc99f565a1efcdb7ad593fa26311e9e150b4f6b
irigyano <110440243+irigyano@users.noreply.github.com>
2024-03-24 00:19:23 +0800

    ci: github actions

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "iriwrap",
-  description: "A VitePress Site"
-})
+  description: "A VitePress Site",
+  base: "/iriwrap/",
+});
